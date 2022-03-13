c8ff9077a72350fd29518570dea808ab4b24352f
mhdzy <mhdzy@users.noreply.github.com>
2022-03-13 15:49:23 -0400

    misc: move autoplay code to player module

--- player/main.py
+++ player/main.py
@@ -1,9 +1,11 @@
+from collections import Counter
+import numpy as np
 import game.PlayGame as pg
 
 MAX_ITER = 10000
 LOG_RATE = 0.10
 games = []
-guesses = []
+guesses = ["score"]
 
 for i in range(0, MAX_ITER):
     if not i % (MAX_ITER * LOG_RATE):
@@ -25,9 +27,12 @@ for i in range(0, MAX_ITER):
 
     games.append(game)
 
-from collections import Counter
-
+# summarised view
 counted = Counter(guesses)
 
-import numpy as np
-np.savetxt('test_10k.csv', guesses, delimiter = ',')
+# save out raw list
+np.savetxt(
+    fname="test_10k.csv",
+    X=guesses,
+    delimiter=",",  
+)
