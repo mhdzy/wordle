d471d13dea96ad8f2f2095b4bdccabc846f39470
mhdzy <mhdzy@users.noreply.github.com>
2022-03-17 21:35:21 -0400

    feat: include feedback generator

---
+++ player/AutoGuess.py
@@ -0,0 +1,17 @@
+outputs: list = []
+def kLengthRec(set, prefix, n, k):
+    global outputs
+    
+    if (k == 0):
+        outputs.append(prefix)
+        return
+ 
+    for i in range(n):
+        newPrefix = prefix + set[i]
+        kLengthRec(set, newPrefix, n, k - 1)
+
+def kLength(set, k):
+    kLengthRec(set, "", len(set), k)
+
+# stores results in 'outputs'
+kLength(['0', '1', '2'], k = 5)
