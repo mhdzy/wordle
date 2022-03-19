88cc755aa4bfdc1134d33f71ba516773b6e53b39
mhdzy <mhdzy@users.noreply.github.com>
2022-03-18 20:56:26 -0400

    feat: implement manual play example

--- player/__main__.py
+++ player/__main__.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     # save out raw list
     np.savetxt(
-        fname="data/test_10k.csv",
+        fname="data/test.csv",
         X=guesses,
         delimiter=",",
     )
