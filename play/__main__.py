1d0e04b9f733fc8010f5858614f7576ce42cb38f
mhdzy <mhdzy@users.noreply.github.com>
2022-04-30 19:25:40 -0400

    tests: implement fractional simulation call in play module

--- play/__main__.py
+++ play/__main__.py
@@ -1,11 +1,13 @@
+
 import game.PlayGame as pg
 import json
 
 game = pg.PlayGame()
 
-#game.autoguess()
-sim_list = game.fb_simulate()
-sim_dict = { s: { k[0]: k[1] for k in sim_list[s] } for s in sim_list }
+QUARTILE: int = 0
 
-with open("data/first-move-simulation-new.json", "w") as fd:
+sim_list: list = game.fb_simulate(quartile = QUARTILE)
+sim_dict: dict = {s: {k[0]: k[1] for k in sim_list[s]} for s in sim_list}
+
+with open("data/first-move-simulation-new-" + QUARTILE + ".json", "w") as fd:
     json.dump(sim_dict, fd, indent=2)
