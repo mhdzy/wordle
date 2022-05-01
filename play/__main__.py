466bac01ed78f35cce6fe0f1de491a5851fe319b
mhdzy <mhdzy@users.noreply.github.com>
2022-04-30 22:04:15 -0400

    feat: final mp tweaks

--- play/__main__.py
+++ play/__main__.py
@@ -1,21 +1,16 @@
-import game.PlayGame as pg
 import play.mpsim as mpsim
+
 import json
+import multiprocessing as mp
 
-from multiprocessing import Pool, cpu_count
+max_threads = int(mp.cpu_count())
+ranges = [i + 1 for i in range(max_threads)]
+pool = mp.Pool(processes=max_threads)
 
-pool = Pool(processes=int(cpu_count()))
+results = pool.map(mpsim.mpsim, ranges)
 
-all_ranges = [i + 1 for i in range(0, int(cpu_count()))]
-
-results = pool.map(mpsim.parallel_wrapper, all_ranges)
-
-part: int = 1
-base: int = 2000
-game = pg.PlayGame()
-
-sim_list: list = game.fb_simulate(partition=part, base=base)
+sim_list: list = {k: v for x in results for k, v in x.items()}
 sim_dict: dict = {s: {k[0]: k[1] for k in sim_list[s]} for s in sim_list}
 
-with open("data/first-move-simulation-new-" + part + ".json", "w") as fd:
+with open("data/first-move-simulation-new-mp.json", "w") as fd:
     json.dump(sim_dict, fd, indent=2)
--- play/mpsim.py
+++ play/mpsim.py
@@ -1,5 +1,6 @@
 import game.PlayGame as pg
 
-def parallel_wrapper(part: int = 0):
-    game = pg.PlayGame()
-    return game.fb_simulate(partition=part, base=12)
+import multiprocessing as mp
+
+def mpsim(partition: int = 0):
+    return pg.PlayGame().fb_simulate(partition=partition, base=mp.cpu_count())
